96c57418abc92530cf349fa1917c64b3b61b73c8
anisingh3012 <anisingh30121998@gmail.com>
2022-12-02 03:27:49 +0530

    First commit

--- src/main/java/com/driver/Book.java
+++ src/main/java/com/driver/Book.java
@@ -10,6 +10,38 @@ public class Book {
      
      private String author;
 
+     public int getId() {
+          return id;
+     }
+
+     public void setId(int id) {
+          this.id = id;
+     }
+
+     public String getName() {
+          return name;
+     }
+
+     public void setName(String name) {
+          this.name = name;
+     }
+
+     public String getGenre() {
+          return genre;
+     }
+
+     public void setGenre(String genre) {
+          this.genre = genre;
+     }
+
+     public String getAuthor() {
+          return author;
+     }
+
+     public void setAuthor(String author) {
+          this.author = author;
+     }
+
      public Book(){
 
      }
